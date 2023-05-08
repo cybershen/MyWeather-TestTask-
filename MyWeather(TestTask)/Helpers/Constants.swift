23129ce60c66c7274f15a530d60108ca774bb265
cybershen <cyberpuer@proton.me>
2023-05-08 14:44:06 +0300

    Add .gitignore file

---
+++ MyWeather(TestTask)/Helpers/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  MyWeather(TestTask)
+//
+//  Created by Назар Жиленко on 02.05.2023.
+//
+
+import Foundation
---
+++ MyWeather(TestTask)/Models/Location.swift
@@ -0,0 +1,8 @@
+//
+//  Location.swift
+//  MyWeather(TestTask)
+//
+//  Created by Назар Жиленко on 06.05.2023.
+//
+
+import Foundation
