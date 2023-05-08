df7848e0389304f53201689bf037fb44b7548327
cybershen <cyberpuer@proton.me>
2023-05-08 15:15:30 +0300

    Fix assets

--- MyWeather(TestTask)/Helpers/Constants.swift
+++ MyWeather(TestTask)/Helpers/Constants.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct Constants {
+    struct API {
+        static let API_KEY = "5d1a5c3311104358afe132406230605"
+        static let baseURL = "https://api.weatherapi.com/v1"
+    }
+    
+    struct MainVCTitle {
+        static let home =  "Home"
+        static let search = "Search"
+    }
+}
--- MyWeather(TestTask)/Models/Location.swift
+++ MyWeather(TestTask)/Models/Location.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct Location: Codable {
+    let name: String
+    let region: String
+    let country: String
+    let lat: Double
+    let lon: Double
+    let tz_id: String
+    let localtime_epoch: Int
+    let localtime: String
+}
